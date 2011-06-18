bf2a4674a0824b376af2cd61bb45041c6f506afa
marc.weistroff <marc.weistroff@sensio.com>
2011-06-18 15:21:10 +0200

    Added xml processing logic

--- lib/Velib/Velib.php
+++ lib/Velib/Velib.php
@@ -25,15 +25,63 @@ class Velib
         return $browser;
     }
 
+    /**
+     * Call Velib webservice and returns details about a station.
+     *
+     * @param integer $id
+     * @return null|array
+     */
     public function stationDetail($id)
     {
         $url = sprintf('%s/%d', $this->stationDetailEntryPoint, $id);
+        $xml = simplexml_load_string($this->browser->get($url)->getContent());
+        if (!$xml) {
+            return null;
+        }
 
-        return $this->browser->get($url)->getContent();
+        $values = array(
+            'available' => (int) $xml->available,
+            'free'      => (int) $xml->free,
+            'total'     => (int) $xml->total,
+            'ticket'    => (int) $xml->ticket,
+        );
+
+        if ($values['available'] === 0 && $values['free'] === 0 && $values['total'] === 0 && $values['ticket'] === 0) {
+            return null;
+        }
+
+        return $values;
     }
 
+    /**
+     * stationList
+     *
+     * @return
+     */
     public function stationList()
     {
-        return $this->browser->get($this->stationListEntryPoint)->getContent();
+        $xml = $this->browser->get($this->stationListEntryPoint)->getContent();
+        $xml = simplexml_load_string($xml);
+        if (!$xml) {
+            return null;
+        }
+
+        $stations = array();
+        foreach ($xml->markers->marker as $marker) {
+            $attributes = $marker->attributes();
+            $station = array(
+                'address'     => (string) $attributes['address'],
+                'bonus'       => (int) $attributes['bonus'],
+                'fullAddress' => (string) $attributes['fullAddress'],
+                'lat'         => (float) $attributes['lat'],
+                'lng'         => (float) $attributes['lng'],
+                'name'        => (string) $attributes['name'],
+                'open'        => (int) $attributes['open'],
+            );
+            $id = (int) $attributes['number'];
+            $stations[$id] = $station;
+        }
+
+        return $stations;
     }
 }
